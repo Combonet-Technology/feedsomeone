bd15c21d5a4ba8bf99441a4f2cc9332fb8f14aa8
Ebenezer <femolak@gmail.com>
2021-10-20 12:04:12 +0200

    user admin mgt

--- user/admin.py
+++ user/admin.py
@@ -6,13 +6,20 @@ from user.models import UserProfile
 # Register your models here.
 @admin.register(UserProfile)
 class ProfileAdmin(ImportExportActionModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone_number', 'state', 'date_joined', 'is_active')
+    list_display = ('first_name', 'last_name', 'phone_number',
+                    'state', 'date_joined', 'is_active')
     list_filter = ('is_active', 'date_joined')
     search_fields = ('state', 'phone_number')
     actions = ['approve_profile', 'disapprove_profile']
 
-    def approve_profile(self, request, queryset):
+    def verify_profile(self, request, queryset):
+        queryset.update(is_verified=True)
+
+    def suspend_profile(self, request, queryset):
+        queryset.update(is_verified=False)
+
+    def enable_user(self, request, queryset):
         queryset.update(is_active=True)
 
-    def disapprove_profile(self, request, queryset):
+    def disable_user(self, request, queryset):
         queryset.update(is_active=False)
