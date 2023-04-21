82af9612593f12b8a3dddede718b36e31a255e31
bcx <femolak@gmail.com>
2023-04-21 11:18:32 +0000

    updates

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 # exit on error
 set -o errexit
 
+# environment setup
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# handle static files
 python manage.py collectstatic --no-input
+
+# migration commands
+python manage.py makemigrations
+python manage.py showmigrations
+python manage.py sqlmigrate
 python manage.py migrate
