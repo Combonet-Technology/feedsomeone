d41b08a5315ceabb7fe651f9927288ad081b790d
bcx <femolak@gmail.com>
2023-04-21 12:45:41 +0000

    added custom user management commands, updated build script

---
+++ management/commands/create_super_user.py
@@ -0,0 +1,16 @@
+import os
+
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username=os.getenv('SUPER_USERNAME'),
+                password=os.getenv('SUPER_USER_PASSWORD')
+            )
+        print('Superuser has been created.')
