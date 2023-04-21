57ef05f095d3baaff7d0f0cb8fc8a8440f4e7579
bcx <femolak@gmail.com>
2023-04-21 20:35:04 +0000

    debugging app on render

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -34,5 +34,3 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-print(DATABASES)
