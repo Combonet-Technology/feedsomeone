ca6e0e96058725e2be46a5800a63049d5033a20c
bcx <femolak@gmail.com>
2023-05-02 11:03:39 +0100

    article admin page, pagination

--- blog/admin.py
+++ blog/admin.py
@@ -1,22 +1,27 @@
 from django.contrib import admin
-from blog.models import Article, Comments, Categories
-from django_summernote.admin import SummernoteModelAdmin
-from import_export.admin import ImportExportActionModelAdmin, ExportActionModelAdmin
+# from django_summernote.admin import SummernoteModelAdmin
+from import_export.admin import ImportExportActionModelAdmin
 
-
-# Register your models here.
-# admin.site.register(Post)
-
-
-# @admin.register(Post)
-# class PostAdmin(ImportExportActionModelAdmin):
-#     pass
+from blog.models import Article, Categories, Comments
 
 
 @admin.register(Article)
 class ArticleAdmin(ImportExportActionModelAdmin):
     # summernote_fields = '__all__'
     summernote_fields = ('post_content',)
+    list_display = ('article_title', 'article_slug', 'article_excerpt', 'article_author', 'status', 'publish')
+    list_filter = ('publish', 'created', 'article_author', 'status')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'article_slug': ('article_title',)}
+    raw_id_fields = ('article_author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
+
+    def publish_article(self, request, queryset):
+        queryset.update(status=True)
+
+    def unpublish_article(self, request, queryset):
+        queryset.update(status=False)
 
 
 @admin.register(Categories)
@@ -27,7 +32,7 @@ class CategoriesAdmin(ImportExportActionModelAdmin):
 
 @admin.register(Comments)
 class CommentAdmin(ImportExportActionModelAdmin):
-    list_display = ('name', 'body', 'post', 'created_on','website', 'active')
+    list_display = ('name', 'body', 'post', 'created_on', 'website', 'active')
     list_filter = ('active', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
