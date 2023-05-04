474f83be8a3dbab2c2512492f2629576a6b380af
bcx <femolak@gmail.com>
2023-05-05 00:05:50 +0100

    added inclusion tags for categories

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('create/', views.create_article, name="create-article"),
     path('all/', ArticleListView.as_view(), name="all-articles"),
     path('all/<slug:tag>', ArticleListView.as_view(), name="articles-by-slug"),
+    path('all/<str:category>', ArticleListView.as_view(), name="articles-by-category"),
     path('<int:pk>/update/', UpdateArticleView.as_view(), name="update-post"),
     path('<int:year>/<int:month>/<int:day>/<slug:slug>/', views.article_detail, name='article_detail'),
     path('<str:username>/', UserArticleListView.as_view(), name="article-by-user"),
