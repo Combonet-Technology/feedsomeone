b6f4fd6422f97aaa4debf16e3b6c093f6105d971
bcx <femolak@gmail.com>
2023-05-05 11:19:51 +0100

    updated pipfile for django-taggit, updated simple tags logic

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from django import template
+from django.db.models import Count
+from django.utils import timezone
 
 from ..models import Article, Categories
 
@@ -6,28 +10,51 @@ register = template.Library()
 
 
 @register.simple_tag(name='total_articles_count')
-def total_posts():
+def total_posts() -> int:
     return Article.published.count()
 
 
 @register.simple_tag(name='total_articles_year')
-def total_annual_posts():
-    return Article.published.count()
+def total_annual_posts(year: Optional[int] = None) -> int:
+    if not year:
+        return Article.published.filter(publish_date__year=timezone.now().year).count()
+    elif year <= timezone.now().year:
+        return Article.published.filter(publish_date__year=year).count()
+    else:
+        return 0
 
 
 @register.simple_tag(name='total_articles_month')
-def total_monthly_posts():
-    return Article.published.count()
+def total_monthly_posts(month: Optional[int] = None) -> int:
+    if not month:
+        return Article.published.filter(publish_date__year=timezone.now().year,
+                                        publish_date__month=timezone.now().month).count()
+    elif month <= 12 and month <= timezone.now().month and month >= 1:
+        return Article.published.filter(publish_date__year=timezone.now().year, publish_date__month=month).count()
+    else:
+        return 0
 
 
 @register.simple_tag(name='total_articles_week')
-def total_weekly_posts():
-    return Article.published.count()
+def total_weekly_posts(week: Optional[int] = None) -> int:
+    if not week:
+        return Article.published.filter(publish_date__year=timezone.now().year,
+                                        publish_date__week=timezone.now().isocalendar()[1]).count()
+    elif week <= 53 and week <= timezone.now().isocalendar()[1] and week >= 1:
+        return Article.published.filter(publish_date__year=timezone.now().year, publish_date__week=week).count()
+    else:
+        return 0
 
 
 @register.simple_tag(name='total_articles_today')
-def total_daily_posts():
-    return Article.published.count()
+def total_daily_posts(date: Optional[timezone.datetime] = None) -> int:
+    if not date:
+        return Article.published.filter(publish_date__year=timezone.now().year,
+                                        publish_date__month=timezone.now().month,
+                                        publish_date__day=timezone.now().day).count()
+    else:
+        return Article.published.filter(publish_date__year=date.year, publish_date__month=date.month,
+                                        publish_date__day=date.day).count()
 
 
 @register.inclusion_tag('blog/latest.html')
@@ -37,6 +64,11 @@ def show_recent_articles(count=5):
 
 
 @register.inclusion_tag('blog/latest.html')
-def show_all_categories(count=5):
+def show_top_categories(count=5):
     categories = Categories.objects.all()[:count]
     return {'categories': categories}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Article.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
