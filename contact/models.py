a51eb95c2ffb16103d1a284d07d7867ac01fa297
bcx <femolak@gmail.com>
2023-05-06 18:02:15 +0100

    contact form revamp

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Contact(models.Model):
     firstname = models.CharField(max_length=40)
@@ -8,4 +9,3 @@ class Contact(models.Model):
     subject = models.CharField(max_length=100)
     message = models.TextField()
     received = models.BooleanField(default=False)
-
