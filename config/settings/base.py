65280fef1279426a449be635b66df62d40bd7aea
bcx <femolak@gmail.com>
2023-05-06 18:46:00 +0100

    added sitemaps features

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.sitemaps',
     'mainsite.apps.MainsiteConfig',
     'blog.apps.BlogConfig',
     'user.apps.UserConfig',
