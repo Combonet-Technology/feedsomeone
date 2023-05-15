213d32d46d890047aa95be4a87f663ec1ea2df95
EBENEZER <9444817+bencipher@users.noreply.github.com>
2023-05-15 21:07:12 +0100

    Update services.py
    Fixed import error

--- user/management/services.py
+++ user/management/services.py
@@ -1,8 +1,8 @@
 from django.contrib.sites.models import Site
 
-from user.management.commands.create_super_user import User
+from django.conf.settings import get_user_model
 
-
+User = get_user_model()
 class SiteService:
     @staticmethod
     def add_site(name, domain):
