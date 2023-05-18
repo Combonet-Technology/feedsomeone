4c3d3c29ccaedbfafa666c344117db36c0e5973f
bcx <femolak@gmail.com>
2023-05-18 16:42:01 +0100

    remove explicit auth declaration in user app

--- config/urls.py
+++ config/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
     path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
+    path('', include('django.contrib.auth.urls')),
     # re_path(r'^', include('cms.urls'))
 ]
 if settings.DEBUG:
--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -8,13 +7,6 @@ urlpatterns = [
     path('register/', views.register, name="register"),
     path('activate/<slug:uidb64>/<slug:token>/', views.activate, name='activate'),
     path('profile/', views.profile, name="profile"),
-    path('login/', auth_views.LoginView.as_view(), name="login"),
     path('view-volunteers/', VolunteerListView.as_view(), name="volunteer-list"),
     path('view-volunteers/<int:pk>/', VolunteerDetailView.as_view(), name="volunteer-details"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
-    path('password-reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
-    path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
-         name="password_reset_confirm"),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
 ]
