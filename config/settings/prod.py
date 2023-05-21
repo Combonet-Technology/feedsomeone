ad8d63abcbc38a6271763642a5cf5507e613b79f
bcx <femolak@gmail.com>
2023-05-21 01:53:07 +0100

    minor

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from .base import *
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
