1032bc24db27c75997ea41e0dc90fb4d0f01e034
bcx <femolak@gmail.com>
2023-06-17 20:26:15 +0100

    Fixed template to make test pass i in TDD

--- mainsite/tests.py
+++ mainsite/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -14,3 +15,11 @@ class HomePage(TestCase):
     def test_root_url_resolves_to_homepage(self):
         home_path = resolve('/')
         self.assertEqual(home_path.func, home)
+
+    def test_homepage_returns_valid_data(self):
+        request = HttpRequest()
+        response = home(request)
+        html_content = response.content.decode('utf-8')
+        self.assertTrue(html_content.startswith('\n\n\n<!doctype html>'))
+        self.assertTrue(html_content.endswith('</html>'))
+        self.assertIn('<title>FEEDSOMEONE | eradicating hunger and poverty</title>', html_content)
