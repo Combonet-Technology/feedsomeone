767ee074a137516cd5045f77eff32e6fc4460b4f
bcx <femolak@gmail.com>
2023-07-07 09:22:50 +0100

    added more mocks for decorator and objects

--- user/mocks.py
+++ user/mocks.py
@@ -1,4 +1,7 @@
-class MockUser:
+from functools import wraps
+
+
+class RegisterUser:
 
     def __init__(self):
         self.username = 'testuser'
@@ -22,3 +25,34 @@ class MockUser:
             'phone_number': self.phone_number,
             'state_of_residence': self.state_of_residence,
         }
+
+
+class MockUser:
+
+    def __init__(self):
+        self.username = 'testuser'
+        self.email = 'testuser@example.com'
+        self.first_name = 'Ben'
+        self.last_name = 'Buffer'
+
+    @property
+    def spawn(self):
+        return {
+            'username': self.username,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+        }
+
+
+def mock_decorator(*args, **kwargs):
+    """Decorate by doing nothing."""
+
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
