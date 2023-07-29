190f1d0caddd6841e4bdb2d50283ab919996ebb7
bcx <femolak@gmail.com>
2023-07-29 01:22:01 +0100

    checked for null email

--- utils/forms.py
+++ utils/forms.py
@@ -3,8 +3,11 @@ from django.core.exceptions import ValidationError
 
 
 def clean_email(email: str) -> bool:
-    domain = 'https://' + email.split('@')[-1]
-    ext = email.split('.')[-1]
+    try:
+        domain = 'https://' + email.split('@')[-1]
+        ext = email.split('.')[-1]
+    except AttributeError:
+        raise ValidationError('Email cannot be blank')
     if ext not in ['org', 'com', 'ng', 'gov', 'ca', 'us']:
         raise ValidationError('Email recipient not supported, use an official, gmail or outlook account')
     try:
