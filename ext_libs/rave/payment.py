332f56cbfb44195f01dcf7a532cba2c59dcbd2e1
bcx <femolak@gmail.com>
2023-09-16 13:22:56 +0100

    updates

--- ext_libs/rave/payment.py
+++ ext_libs/rave/payment.py
@@ -5,15 +5,20 @@ from typing import Optional
 
 import requests
 from django.shortcuts import redirect
+from dotenv import load_dotenv
 
 from rave_python import Rave
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..')))
 from utils.enums import SubscriptionPlan  # noqa: E402
 
-FLW_SECRET_KEY = os.getenv("FLW_SECRET_KEY")
+load_dotenv()
+FLW_SECRET_KEY = os.environ.get("RAVE_SECRET_KEY")
+FLW_PUBLIC_KEY = os.environ.get("RAVE_PUBLIC_KEY")
+ENCRYPTION_KEY = os.environ.get("FLW_ENCRYPTION_KEY")
+print(FLW_PUBLIC_KEY, FLW_SECRET_KEY)
+TEST_FLW_PUBLIC_KEY = 'FLWPUBK_TEST-1db61213ff9e3721f770e0697a9e60c5-X'
 TEST_FLW_SECRET_KEY = 'FLWSECK_TEST-8017fa700f790646a835491cefd646c7-X'
-TEST_PUBLIC_KEY = 'FLWPUBK_TEST-1db61213ff9e3721f770e0697a9e60c5-X'
 TEST_ENCRYPTION_KEY = 'FLWSECK_TEST73746dfa2c5a'
 TEST_RAVE_WEBHOOK_URL = 'https://dev.oluwafemiebenezer.foundation/webhooks'
 RAVE_WEBHOOK_URL = TEST_RAVE_WEBHOOK_URL
@@ -74,7 +79,7 @@ def generate_payment_link(amount, currency, customer_data, tx_ref_id, payment_pl
 
 def create_subscription_plan(duration: SubscriptionPlan):
     try:
-        rave = Rave(os.getenv("FLW_PUBLIC_KEY"), os.getenv("FLW_SECRET_KEY"))
+        rave = Rave(FLW_PUBLIC_KEY, FLW_SECRET_KEY)
         res = rave.PaymentPlan.create({
             "name": f"{duration.capitalize()} Subscription Plan",
             "interval": duration
@@ -93,7 +98,7 @@ if __name__ == '__main__':
     # if payment_url:
     #     print("Payment plan created successfully.")
     #
-    # subscription_plan_result = create_subscription_plan("monthly")
-    # if subscription_plan_result:
-    #     print("Subscription plan created successfully.")
-    generate_payment_link(10000, 'NGN', customer_data.__dict__, '123werty-feed')
+    subscription_plan_result = create_subscription_plan("monthly")
+    if subscription_plan_result:
+        print("Subscription plan created successfully.")
+    # generate_payment_link(10000, 'NGN', customer_data.__dict__, '123werty-feed')
